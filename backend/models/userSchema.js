e28e01af2345ec30780b51ac56f833c4f59a171d
Shresthanirjala <nirjalas437@gmail.com>
2025-02-12 16:14:27 +0545

    password hashing

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -54,6 +54,13 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")){
+    next()
+  }
+  this.password = await bcrypt.hash(this.password,10);
+})
+
 userSchema.methods.getJWTToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRE,
