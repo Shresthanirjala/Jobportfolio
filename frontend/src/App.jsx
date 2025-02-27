2afe360877f3e57b381335f513d88aa4b8cbeb35
Shresthanirjala <nirjalas437@gmail.com>
2025-02-27 16:55:38 +0545

    frontend routing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Fixed import
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home"; // Corrected import
+import Jobs from "./pages/Jobs";
+import Dashboard from "./pages/Dashboard";
+import PostApplication from "./pages/PostApplication";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Register from "./pages/Register";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <Navbar/>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/post/application/:jobId" element={<PostApplication />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
