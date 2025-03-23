4fe2ecdbad2584dbd7fed54aa7f8e9ef410821b6
Shresthanirjala <nirjalas437@gmail.com>
2025-03-23 22:34:23 +0545

    feat: about

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className="mt-18px-4 md:px-10 pt-24 py-[100px]">
-      h
-    </div>
-  )
-}
+    <div className="mt-18px-4 md:px-40 pt-24 py-40">
+      <h1 className="text-[14px] text-[#023552] mt-6">About Handy Hire</h1>
 
-export default About
+      <div className="flex flex-row gap-5">
+        <div>
+          <img src="/images/about1.png" />
+        </div>
+        <div>
+          <h1 className="text-xl text-[#023552] font-bold">WHO WE ARE </h1>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default About;
